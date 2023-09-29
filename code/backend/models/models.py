103f850c57c04ca7e553a95394f3afa5327ee74e
Amol Borkar <amolborkar5@gmail.com>
2023-09-29 15:51:49 -0400

    import changes

--- code/backend/models/models.py
+++ code/backend/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Numeric, Date, Float
+from sqlalchemy import create_engine, Column, Integer, String, Numeric, Float
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
