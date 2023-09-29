8af08be02b9a54e002af6e1d905a4fc35c3dedf4
francoarenas91 <96075641+francoarenas91@users.noreply.github.com>
2023-09-29 16:07:17 -0400

    create tables fix

--- code/backend/models/create_tables.py
+++ code/backend/models/create_tables.py
@@ -4,10 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import inspect
 
 # Modify this path to your DB
-db_uri = 'sqlite:///C:/Users/caleb/poker.db'
+db_uri = 'sqlite:///C:/Users/franc/poker'
 engine = create_engine(db_uri)
-
-Base = declarative_base()
 
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
@@ -25,3 +23,4 @@ if not missing_tables:
     print("All expected model tables exist in the database.")
 else:
     print("The following tables are missing from the database:", missing_tables)
+
--- code/backend/models/models.py
+++ code/backend/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Numeric, Float
+from sqlalchemy import create_engine, Column, Integer, String, Numeric, Float, Date
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
