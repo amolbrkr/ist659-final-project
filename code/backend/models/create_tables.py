6749b6567bcb404ba15a62146b04d8c57a63dfbf
Amol Borkar <amolborkar5@gmail.com>
2023-09-29 15:58:44 -0400

    added create table script

---
+++ code/backend/models/create_tables.py
@@ -0,0 +1,27 @@
+from models import *
+from sqlalchemy import create_engine, Column, Integer, String, Numeric, Date, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import inspect
+
+# Modify this path to your DB
+db_uri = 'sqlite:///C:/Users/caleb/poker.db'
+engine = create_engine(db_uri)
+
+Base = declarative_base()
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+
+expected_table_names = ['cards', 'player', 'pStats', 'match', 'lobby', 'matchDet', 'login']
+
+inspector = inspect(engine)
+
+table_names = inspector.get_table_names()
+
+missing_tables = [table_name for table_name in expected_table_names if table_name not in table_names]
+
+if not missing_tables:
+    print("All expected model tables exist in the database.")
+else:
+    print("The following tables are missing from the database:", missing_tables)
