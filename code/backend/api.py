3c87e06f180f77be9306b2129a6c187820aff1fc
francoarenas91 <96075641+francoarenas91@users.noreply.github.com>
2023-10-04 16:51:50 -0400

    database in girthub

--- code/backend/api.py
+++ code/backend/api.py
@@ -5,9 +5,14 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import IntegrityError
+import os
+
+#set directory
+script_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(os.path.dirname(os.path.dirname(script_directory)))
 
 # Define SQLAlchemy database connection
-DATABASE_URL = "sqlite:///C:/Users/franc/poker"  # Replace with your database URL
+DATABASE_URL = "sqlite:///database/poker"  # Replace with your database URL
 engine = create_engine(DATABASE_URL)
 db = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
 
@@ -17,21 +22,6 @@ app = FastAPI()
 # Create request models using Pydantic
 class LobbyCreate(BaseModel):
     lobby_name: str
-
-# @app.post("/create-lobby/", response_model=Lobby)
-# async def create_lobby(lobby: LobbyCreate):
-
-#     try:
-#         # Create a new lobby in the database
-#         new_lobby = Lobby(**lobby.dict())
-#         db.add(new_lobby)
-#         db.commit()
-#         db.refresh(new_lobby)
-#         return new_lobby
-#     except IntegrityError:
-#         raise HTTPException(status_code=400, detail="Lobby name already exists")
-#     finally:
-#         db.close()
 
 # Create request models using Pydantic
 class PlayerCreate(BaseModel):
