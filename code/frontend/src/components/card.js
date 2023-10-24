90295e47e8cca9eb43f786665d483673eda930d0
madamore-1 <madamore@syr.edu>
2023-10-24 18:11:20 -0400

    btbttbt

--- code/frontend/src/components/card.js
+++ code/frontend/src/components/card.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React from 'react';
 
-const Card = ({ card }) => {
+function SvgCards() {
   return (
     <div>
+      <h1>Example SVG with cards</h1>
       <svg
-        width="300"
-        height="200"
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        fill={fillColor}
+        class="bi bi-google"
+        viewBox="0 0 16 16"
       >
-        <use href="svg-cards.svg#back" x="150" y="10" fill="red" />
+        {/* Your SVG content here */}
+        <use href="node_modules/svg-cards/svg-cards.svg#back" x="150" y="10" fill="red" />
+        <use href="node_modules/svg-cards/svg-cards.svg#heart_1" x="0" y="0" />
+        <use href="node_modules/svg-cards/svg-cards.svg#joker_black" x="100" y="100" />
+        <use
+          href="node_modules/svg-cards/svg-cards.svg#spade_10"
+          x="200"
+          y="200"
+          transform="rotate(45,198.0375,122.320)scale(0.5)"
+        />
+        <use
+          href="svg-cards.svg#club_jack"
+          x="300"
+          y="100"
+          transform="rotate(75,198.0375,122.320)scale(0.75)"
+        />
       </svg>
     </div>
   );
-};
+}
 
-export default Card;
+export default SvgCards;
+  
